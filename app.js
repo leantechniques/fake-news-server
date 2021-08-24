9ece8dd20c47c2f18e8c3b70c8f3889093d5dd09
Nate Johnson <njohnson@107-c02yp55hlvch.lan>
2021-08-24 15:59:57 -0500

    enabled cors on backend

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 const Chance = require("chance");
 const axios = require("axios");
+const cors = require('cors');
+app.use(cors());
+app.options("*", cors());
 
 const chance = new Chance();
 
